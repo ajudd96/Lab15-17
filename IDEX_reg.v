21a6d5698180cc3f3ebb8308afac3d2d0d0faaf5
vreyes11 <32913916+vreyes11@users.noreply.github.com>
2017-11-01 15:04:23 -0700

    added mem control signals to declerations

--- IDEX_reg.v
+++ IDEX_reg.v
@@ -9,9 +9,9 @@ module IDEX_reg(
 //				Control Lines;
 					ID_RegWrite,
 					ID_CondMov,
-					MemtoReg,
-					MemWrite,
-					MemRead,
+					ID_MemtoReg,
+					ID_MemWrite,
+					ID_MemRead,
 					ID_RegDst,
 					ID_ALUOp,
 					ID_ALUSrc1,
@@ -51,14 +51,14 @@ module IDEX_reg(
 
 
 input Clk;
-input ID_RegWrite, ID_CondMov, ID_RegDst, ID_ALUSrc1, ID_ALUSrc2;
+input ID_RegWrite, ID_CondMov, ID_RegDst, ID_ALUSrc1, ID_ALUSrc2, ID_MemtoReg, ID_MemWrite, ID_MemRead;
 input wire [4:0] ID_ALUOp;
-input wire [31:0] ID_ReadData1, ID_ReadData2, ID_immExt, ID_sa;
+input wire [31:0] ID_PCAddResult, ID_ReadData1, ID_ReadData2, ID_immExt, ID_sa;
 input wire [4:0] ID_rt, ID_rd;
 
-output reg EX_RegWrite, EX_CondMov, EX_RegDst, EX_ALUSrc1, EX_ALUSrc2;
+output reg EX_RegWrite, EX_CondMov, EX_RegDst, EX_ALUSrc1, EX_ALUSrc2, EX_MemtoReg, EX_MemWrite, EX_MemRead;
 output reg [4:0] EX_ALUOp;
-output reg [31:0] EX_ReadData1, EX_ReadData2, EX_immExt, EX_sa;
+output reg [31:0] EX_PCAddResult, EX_ReadData1, EX_ReadData2, EX_immExt, EX_sa;
 output reg [4:0] EX_rt, EX_rd;
 
 
@@ -67,14 +67,14 @@ always @(posedge Clk) begin
 	
 	// Controls
 	EX_RegWrite <= ID_RegWrite;
-	EX_MemtoReg <= MemtoReg;
-	EX_MemWrite <= MemWrite;
-	EX_MemRead <= MemRead;
+	EX_MemtoReg <= ID_MemtoReg;
+	EX_MemWrite <= ID_MemWrite;
+	EX_MemRead <= ID_MemRead;
 	EX_RegDst <= ID_RegDst;
 	EX_ALUSrc1 <= ID_ALUSrc1;
 	EX_ALUSrc2 <= ID_ALUSrc2;
 	EX_ALUOp <= ID_ALUOp;
-    EX_CondMov <= ID_CondMov;
+        EX_CondMov <= ID_CondMov;
 	// Data
 	EX_PCAddResult <= ID_PCAddResult;
 	EX_ReadData1 <= ID_ReadData1;
